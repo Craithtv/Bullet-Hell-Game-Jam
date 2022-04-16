b8e612900e48b593c66ff6d7c8335463425b2c4b
AquosC5 <56211736+AquosC5@users.noreply.github.com>
2022-04-16 14:53:26 -0400

    starting on dash

--- BulletHell/Assets/Scripts/PlayerMovement.cs
+++ BulletHell/Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public Vector2 dashSpeed;
 
-    [SerializeField] private LayerMask wallLayers;
     [SerializeField] private Camera mainCamera;
     private Vector3 mousePos;
     private SpriteRenderer playerSprite;
-
+    private Rigidbody2D player;
 
 
     private void Start()
     {
         playerSprite = GetComponent<SpriteRenderer>();
+        player = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
 
         gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+
+        //Dash code, not working yet
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(player.velocity.x * 30, player.velocity.y * 30), ForceMode2D.Impulse);
+        }
 
     }
 
@@ -30,16 +37,14 @@ public class PlayerMovement : MonoBehaviour
     {
         transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
         //GetAxisRaw disables ease in
-        
+
+
 
         mousePos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
 
         //Find where the mouse is relative to player position then rotate player
         Vector3 difference = mousePos - gameObject.transform.position;
         
-        //This would rotate the player, but trying out rotate weapon instead of player so this code may not be needed
-        //float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        //gameObject.transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
 
         //Flips the sprite on X-axis when mouse is on the left side of player
         if (difference.x < 0)
