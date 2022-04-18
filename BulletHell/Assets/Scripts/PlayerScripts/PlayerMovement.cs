978f570d176f5b6bb700fec08d8295119eaaceb6
AquosC5 <56211736+AquosC5@users.noreply.github.com>
2022-04-18 17:58:36 -0400

    Revert "Delete PlayerMovement.cs"
    This reverts commit ddaf53ea903693a6fac281108cf32a0b6801f2b5.

---
+++ BulletHell/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    float baseSpeed;
+    float dashCooldown;
+
+    BoxCollider2D coll;
+    GameObject[] bullets;
+
+    [SerializeField] private LayerMask wallLayer;
+
+    Vector3 dir;
+
+    Vector3 playerDir;
+
+    enum PlayerState
+    {
+        Normal,
+        Dash
+    };
+
+    private PlayerState playerState;
+
+    [SerializeField] private Camera mainCamera;
+    private Vector3 mousePos;
+    private SpriteRenderer playerSprite;
+
+
+    private void Start()
+    {
+        playerSprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
+        baseSpeed = speed;
+        playerState = PlayerState.Normal;
+    }
+
+    private void Update()
+    {
+        FlipSprite();
+
+        dir = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCooldown <= 0 && CanMove())
+        {
+            speed *= 5f;
+            dashCooldown = 0.5f;
+            playerState = PlayerState.Dash;
+        }
+
+
+        if (playerState == PlayerState.Dash)
+        {
+            bullets = GameObject.FindGameObjectsWithTag("Bullet");
+
+            for (int i = 0; i < bullets.Length; i++)
+            {
+                Physics2D.IgnoreCollision(coll, bullets[i].GetComponent<CircleCollider2D>());
+            }
+
+            transform.position += dir * speed * Time.deltaTime;
+            speed -= speed * 10f * Time.deltaTime;
+
+            if (speed < baseSpeed)
+            {
+                playerState = PlayerState.Normal;
+                speed = baseSpeed;
+            }
+        }
+
+        if (dashCooldown > 0)
+        {
+            dashCooldown -= Time.deltaTime;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerState == PlayerState.Normal)
+            transform.position += dir * speed * Time.deltaTime;
+    }
+
+    void FlipSprite()
+    {
+        mousePos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        
+        //Find where the mouse is relative to player position then rotate player
+        Vector3 difference = mousePos - gameObject.transform.position;
+
+
+        //Flips the sprite on X-axis when mouse is on the left side of player
+        if (difference.x < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        else
+        {
+            playerSprite.flipX = false;
+        }
+
+    }
+
+
+    //Check if there is a wall in front of player
+    //Distance may need to be adjusted based on player/collider size
+    bool CanMove()
+    {
+        return Physics2D.Raycast(transform.position, dir, 1.5f, wallLayer).collider == null;
+    }
+
+}
