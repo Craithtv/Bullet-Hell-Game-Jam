eadc20299215f39524c450a9519be3d95ee06660
AquosC5 <56211736+AquosC5@users.noreply.github.com>
2022-04-18 17:15:52 -0400

    new dash and invincibility frames
    i-frames in the form of being in dash state, very brief window. makes an array that checks for each bullet and ignores (may need a better way since this might be taxing)

--- BulletHell/Assets/Scripts/PlayerHeartManager.cs
+++ BulletHell/Assets/Scripts/PlayerHeartManager.cs
@@ -29,7 +29,7 @@ public class PlayerHeartManager : MonoBehaviour
         {
             if(player.GetPlayerHp()-1 < i)
             {
-                Destroy(renderedHearts[i]);
+                renderedHearts[i].SetActive(false);
                 
             }
         }
@@ -39,20 +39,25 @@ public class PlayerHeartManager : MonoBehaviour
     {
         int drawnHearts = 0;
         Vector3 topLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, 0));
+        GameObject obj;
         for(int i = 0; i < player.startHp; i++)
         {
-
             switch (i%2)
             {
                 case (0):
-                    Instantiate(heartSprites[0], topLeft + new Vector3((heartSprites[i%3].GetComponent<SpriteRenderer>().bounds.size.x + (heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x * drawnHearts)), -heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.y, 3), Quaternion.Euler(0f, 0f, 0f));
-                    renderedHearts[i] = Instantiate(heartSprites[1], topLeft + new Vector3(heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x + (heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x * drawnHearts), -heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.y, 2), Quaternion.Euler(0f, 0f, 0f));
+                    obj = Instantiate(heartSprites[0], topLeft + new Vector3((heartSprites[i%3].GetComponent<SpriteRenderer>().bounds.size.x + (heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x * drawnHearts)), -heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.y, 3), Quaternion.Euler(0f, 0f, 0f));
+                    obj.transform.parent = mainCamera.transform;
+
+                    obj= renderedHearts[i] = Instantiate(heartSprites[1], topLeft + new Vector3(heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x + (heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x * drawnHearts), -heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.y, 2), Quaternion.Euler(0f, 0f, 0f));
+                    obj.transform.parent = mainCamera.transform;
                     break;
                 case (1):
-                    renderedHearts[i] = Instantiate(heartSprites[2], topLeft + new Vector3(heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x + (heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x * drawnHearts), -heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.y, 1), Quaternion.Euler(0f, 0f, 0f));
+                    obj = renderedHearts[i] = Instantiate(heartSprites[2], topLeft + new Vector3(heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x + (heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.x * drawnHearts), -heartSprites[i % 3].GetComponent<SpriteRenderer>().bounds.size.y, 1), Quaternion.Euler(0f, 0f, 0f));
+                    obj.transform.parent = mainCamera.transform;
                     drawnHearts += 1;
                     break;
             }
+            
         }
     }
 }
--- BulletHell/Assets/Scripts/PlayerMovement.cs
+++ BulletHell/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     float baseSpeed;
-    float dashSpeed;
-    float dashCooldown = 0;
-    bool canRoll;
-    [SerializeField] private LayerMask wallLayers;
+    float dashCooldown;
+
+    BoxCollider2D coll;
+    GameObject[] bullets;
+
+    [SerializeField] private LayerMask wallLayer;
+
+    Vector3 dir;
+
+    Vector3 playerDir;
 
     enum PlayerState
     {
-        Walk,
+        Normal,
         Dash
     };
 
@@ -22,50 +28,69 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Camera mainCamera;
     private Vector3 mousePos;
     private SpriteRenderer playerSprite;
-    private Rigidbody2D player;
 
 
     private void Start()
     {
         playerSprite = GetComponent<SpriteRenderer>();
-        player = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         baseSpeed = speed;
-        dashSpeed = speed * 10;
-        playerState = PlayerState.Walk;
+        playerState = PlayerState.Normal;
     }
 
     private void Update()
     {
-        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        direction = direction.normalized;
+        FlipSprite();
 
-        switch (playerState)
+        dir = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCooldown <= 0 && CanMove())
         {
-            case PlayerState.Walk:
-
-                Movement();
-                if(Input.GetKeyDown(KeyCode.LeftShift) && CanMove(direction, 1.5f))
-                    Dash();
-                break;
-
-            case PlayerState.Dash:
-                DashSlide(direction);
-                break;
+            speed *= 5f;
+            dashCooldown = 0.5f;
+            playerState = PlayerState.Dash;
         }
+
+
+        if (playerState == PlayerState.Dash)
+        {
+            bullets = GameObject.FindGameObjectsWithTag("Bullet");
+
+            for (int i = 0; i < bullets.Length; i++)
+            {
+                Physics2D.IgnoreCollision(coll, bullets[i].GetComponent<CircleCollider2D>());
+            }
+
+            transform.position += dir * speed * Time.deltaTime;
+            speed -= speed * 10f * Time.deltaTime;
+
+            if (speed < baseSpeed)
+            {
+                playerState = PlayerState.Normal;
+                speed = baseSpeed;
+            }
+        }
+
+        if (dashCooldown > 0)
+        {
+            dashCooldown -= Time.deltaTime;
+        }
+
     }
 
     private void FixedUpdate()
     {
-        
+        if (playerState == PlayerState.Normal)
+            transform.position += dir * speed * Time.deltaTime;
     }
 
-    void Movement()
+    void FlipSprite()
     {
-        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
-        //GetAxisRaw disables ease in
-
         mousePos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-
+        
         //Find where the mouse is relative to player position then rotate player
         Vector3 difference = mousePos - gameObject.transform.position;
 
@@ -79,133 +104,15 @@ public class PlayerMovement : MonoBehaviour
         {
             playerSprite.flipX = false;
         }
-    }
-
-    void Dash()
-    {
-        speed = dashSpeed;
-        playerState = PlayerState.Dash;
-    }
-
-    void DashSlide(Vector3 slideDir)
-    {
-        transform.position += slideDir * speed * Time.deltaTime;
-        player.GetComponent<BoxCollider2D>().enabled = false;
-
-        speed -= speed * 10f * Time.deltaTime;
-        if(speed < baseSpeed)
-        {
-            speed = baseSpeed;
-            player.GetComponent<BoxCollider2D>().enabled = true;
-            playerState = PlayerState.Walk;
-        }
-    }
-
-    bool CanMove(Vector3 dir, float distance)
-    {
-        return Physics2D.Raycast(transform.position, dir, distance, wallLayers).collider == null;
-    }
-
-    bool TryMove(Vector3 baseMoveDir, float distance)
-    {
-        Vector3 moveDir = baseMoveDir;
-        bool canMove = CanMove(moveDir, distance);
-        if (!canMove)
-        {
-            moveDir = new Vector3(baseMoveDir.x, 0f).normalized;
-            canMove = moveDir.x != 0f && CanMove(moveDir, distance);
-            if (!canMove)
-            {
-                moveDir = new Vector3(0f, baseMoveDir.y).normalized;
-                canMove = moveDir.y != 0f && CanMove(moveDir, distance);
-            }
-        }
-
-        if (canMove)
-        {
-            transform.position += moveDir * distance;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
 
     }
 
-    /*private void Update()
+
+    //Check if there is a wall in front of player
+    //Distance may need to be adjusted based on player/collider size
+    bool CanMove()
     {
-
-        gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            player.AddForce(new Vector2(speed, speed), ForceMode2D.Force);
-            playerState = PlayerState.Dash;
-            Debug.Log(playerState);
-        }
-
-        if (playerState == PlayerState.Dash)
-        {
-            speed = dashSpeed;
-            dashCooldown = 0.5f;
-            gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            playerState = PlayerState.Walk;
-        }
-
-
-        //Scuffed dash code
-        if (dashCooldown > 0)
-        {
-            dashCooldown -= Time.deltaTime;
-        }
-
-        if (speed > baseSpeed)
-        {
-            speed -= 100 * Time.deltaTime;
-        }
-
-        else
-        {
-            playerState = PlayerState.Walk;
-            gameObject.GetComponent<BoxCollider2D>().enabled = true;
-            speed = baseSpeed;
-        }
-
+        return Physics2D.Raycast(transform.position, dir, 1.5f, wallLayer).collider == null;
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-
-        if(playerState == PlayerState.Walk)
-        {
-            transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
-            //GetAxisRaw disables ease in
-
-
-
-            mousePos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-
-            //Find where the mouse is relative to player position then rotate player
-            Vector3 difference = mousePos - gameObject.transform.position;
-
-
-            //Flips the sprite on X-axis when mouse is on the left side of player
-            if (difference.x < 0)
-            {
-                playerSprite.flipX = true;
-            }
-            else
-            {
-                playerSprite.flipX = false;
-            }
-        }
-    }
-
-    bool canMove(Vector3 dir, float distance)
-    {
-        return Physics2D.Raycast(transform.position, dir, distance, wallLayers).collider == null;
-    }*/
 
 }
